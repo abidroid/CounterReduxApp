a915b52ba74a14fdad5d4a38f308e66565084bbe
Muhammad Abid <abidroid1@gmail.com>
2025-06-09 17:42:04 +0500

    fixes

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+
+const App = () => {
+ 
+  return (
+    <View>
+      
+      <Text>Welcome</Text>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
