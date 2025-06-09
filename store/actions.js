2e528b2bcf5e67865bf873687799200ec02a32e0
Muhammad Abid <abidroid1@gmail.com>
2025-06-09 18:34:02 +0500

    actions

---
+++ store/actions.js
@@ -0,0 +1,5 @@
+export const INCREMENT = 'INCREMENT';
+export const DECCREMENT = 'DECREMENT';
+
+export const increment = () => ({type: INCREMENT});
+export const decrement = () => ({type: DECCREMENT});
